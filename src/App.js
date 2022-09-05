65e33f1afed707eabe2f51a938ce82df5979141c
Max Powell <maxfpowell@gmail.com>
2022-09-05 21:18:15 +0100

    4.75-79 | Set up categories for home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryList from "./category-list/category-list.component";
+import { CATEGORIES } from "./categories";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <CategoryList categories={CATEGORIES} />;
+};
 
 export default App;
