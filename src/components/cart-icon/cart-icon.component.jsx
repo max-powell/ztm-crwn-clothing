8b295fdfd70090f88809898617ce5d2a7b1aab5b
Max Powell <maxfpowell@gmail.com>
2022-09-30 15:16:03 +0100

    9.115 | Added cart icon and dropdown components

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,12 @@
+import { ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = () => (
+  <div className='cart-icon-container'>
+    <ShoppingBagIcon className='shopping-icon' />
+    <span className='item-count'>0</span>
+  </div>
+);
+
+export default CartIcon;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,9 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.util';
 
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -29,7 +32,9 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
