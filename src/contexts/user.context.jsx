d61ee1e5fdd8194ebd7287ce97b8c521c283ca39
Max Powell <maxfpowell@gmail.com>
2022-10-10 21:03:29 +0100

    13.148 | Updated user context to use createAction util

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -4,6 +4,7 @@ import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from '../utils/firebase/firebase.util';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 export const UserContext = createContext({
   currentUser: null,
@@ -30,10 +31,8 @@ const INITIAL_STATE = { currentUser: null };
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
   const setCurrentUser = (user) => {
-    dispatch({
-      type: USER_ACTION_TYPES.SET_CURRENT_USER,
-      payload: user,
-    });
+    const action = createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
+    dispatch(action);
   };
 
   useEffect(() => {
