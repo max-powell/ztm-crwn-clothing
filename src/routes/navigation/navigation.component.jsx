531c78f2a814d461883a1e1c8277f038e61cec6b
Max Powell <maxfpowell@gmail.com>
2022-10-10 20:46:29 +0100

    13.145 | Removed console log

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,7 +13,6 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
   const { cartIsOpen } = useContext(CartContext);
 
   return (
